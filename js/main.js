98ff523745cfb1c635aee659c165a8e62a15261b
arobillard <robi0653@algonquinlive.com>
2018-03-10 11:04:37 -0500

    Fix naming issue

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var revealMessage = function () {
+
+  var hiddenMessage = '<p><span>&#9734;</span>Superstar!</p>';
+  var i;
+
+  for (i = 0; i < 10; i++) {
+    document.write(hiddenMessage);
+  };
+
+};
+
+revealMessage();
